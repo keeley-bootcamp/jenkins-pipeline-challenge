2646e0271e4b4741cffc31c18f89b81018ca1205
keeley-bootcamp <keeley.bootcamp@gmail.com>
2024-11-07 13:32:55 +0000

    Update App.java

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args ) {
        addInts(100, 200);
+       addInts(99, 2453);
+       addInts(4, 15);
+       addInts(19, 228);
     }
 
     public static void addInts(int a, int b) {
